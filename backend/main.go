12d5e5d8fc1df24feddcb618ad01e11b48716f0e
Santiago <santipuenlo@gmail.com>
2026-09-10 15:41:25 -0500

    Fix frontend backend CORS configuration

--- backend/main.go
+++ backend/main.go
@@ -23,7 +23,7 @@ type ErrorResponse struct {
 func calculateHandler(w http.ResponseWriter, r *http.Request) {
 
 	// Permitir solicitudes desde el frontend
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5174")
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	w.Header().Set("Content-Type", "application/json")
